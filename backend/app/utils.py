fc5937484ab61bb914cf7e042439401831f823b2
PGTigerB <90287289+PGTigerB@users.noreply.github.com>
2025-03-04 12:21:25 -0600

    added utils.py
    Has Shortest Path Algorithms

---
+++ backend/app/utils.py
@@ -0,0 +1,12 @@
+import networkx as nx
+
+def find_shortest_path(graph, source, target, algorithm='dijkstra'):
+  
+    if algorithm == 'dijkstra':
+        return nx.shortest_path(graph, source=source, target=target, weight='weight')
+    elif algorithm == 'astar':
+        return nx.astar_path(graph, source=source, target=target, weight='weight')
+    elif algorithm == 'bidirectional':
+        return nx.bidirectional_dijkstra(graph, source, target, weight='weight')[1]
+    else:
+        raise ValueError("Invalid algorithm. Choose 'dijkstra', 'astar', or 'bidirectional'.")
