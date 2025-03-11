6f211de94dc50f0ece100018969b3516ee7db61f
JoePanthalani <joseph.panthalani@gmail.com>
2025-03-11 02:01:35 -0400

    Add traveling salesman path function

--- backend/app/utils.py
+++ backend/app/utils.py
@@ -10,3 +10,7 @@ def find_shortest_path(graph, source, target, algorithm='dijkstra'):
         return nx.bidirectional_dijkstra(graph, source, target, weight='weight')[1]
     else:
         raise ValueError("Invalid algorithm. Choose 'dijkstra', 'astar', or 'bidirectional'.")
+#Link for traveling_salesman_problem function in NetworkX: https://networkx.org/documentation/stable/reference/algorithms/generated/networkx.algorithms.approximation.traveling_salesman.traveling_salesman_problem.html#networkx.algorithms.approximation.traveling_salesman.traveling_salesman_problem
+def traveling_salesman_path(graph):
+	tsp_path = nx.approximation.traveling_salesman_problem(graph, weight='weight',cycle=True)
+	return tsp_path
