05f8cf7721ab00bff9357e1e787b84e324370530
PGTigerB <90287289+PGTigerB@users.noreply.github.com>
2025-03-12 12:24:05 -0500

    Update utils.py
    updated tsp to include more options and added complete_graph that takes an incomplete graph and makes it complete

--- backend/app/utils.py
+++ backend/app/utils.py
@@ -1,6 +1,4 @@
 import networkx as nx
-import pandas as pd
-import matplotlib.pyplot as plt
 
 def find_shortest_path(graph, source, target, algorithm='dijkstra'):
   
@@ -13,3 +11,35 @@ def find_shortest_path(graph, source, target, algorithm='dijkstra'):
     else:
         raise ValueError("Invalid algorithm. Choose 'dijkstra', 'astar', or 'bidirectional'.")
 
+#Link for traveling_salesman_problem function in NetworkX: https://networkx.org/documentation/stable/reference/algorithms/generated/networkx.algorithms.approximation.traveling_salesman.traveling_salesman_problem.html#networkx.algorithms.approximation.traveling_salesman.traveling_salesman_problem
+def traveling_salesman_path(graph, method='asadpour'):
+   
+    #DON'T use greedy when the graph was orgnially in-complete
+    if method == 'greedy':
+        tsp_path = nx.approximation.greedy_tsp(graph, weight="weight")
+    elif method == 'simulated_annealing':
+        tsp_path = nx.approximation.simulated_annealing_tsp(graph, init_cycle="greedy",  weight="weight", max_iterations=500)
+    elif method == 'threshold_accepting':
+        tsp_path = nx.approximation.threshold_accepting_tsp(graph, init_cycle="greedy",weight="weight",max_iterations=500)
+    elif method == 'asadpour':
+        tsp_path = nx.approximation.traveling_salesman_problem(graph, weight='weight', cycle=True)
+    else:
+        raise ValueError("Invalid TSP method. Choose 'greedy', 'simulated_annealing', 'threshold_accepting', or 'asadpour'.")
+
+    return tsp_path
+
+def complete_graph(graph):
+    penalty=1e9
+    nodes = list(graph.nodes)
+    complete_graph = nx.complete_graph(len(nodes), create_using=nx.DiGraph())
+    
+    for j in nodes:
+        for k in nodes:
+            if  j != k:
+                if graph.has_edge(j, k):
+                    weight = graph[j][k]['weight']
+                else:
+                    weight = penalty
+                complete_graph.add_edge(j,k, weight=weight)
+    
+    return complete_graph
