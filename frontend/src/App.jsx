39fac3d1964c32e8e5d3d968def2a49ff2a98940
bmgb5h <bmgb5h@mst.edu>
2025-04-02 12:36:20 -0500

    add package-lock.json back

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import GraphBuilder from "./components/GraphBuilder";
+import GraphViewer from "./components/GraphViewer";
 
 const App = () => {
   return (
-    <div>
-      <GraphBuilder />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<GraphBuilder />} />
+        <Route path="/graph/:graphId" element={<GraphViewer />} />
+      </Routes>
+    </Router>
   );
 }
 
