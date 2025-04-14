42d035438b238f21568d354e93f2198133c07e70
bmgb5h <bmgb5h@mst.edu>
2025-04-14 10:53:21 -0500

    change ids to use uuid and add tsp route

--- backend/app/models/graph.py
+++ backend/app/models/graph.py
@@ -4,7 +4,7 @@ from app.extensions import db
 class Graph(BaseModel):
     __tablename__ = 'graphs'
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.String(36), primary_key=True)
     name = db.Column(db.String(80), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     data = db.Column(db.JSON, nullable=False)
--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,6 +1,6 @@
 from uuid import uuid4
 import networkx as nx
-from flask import Blueprint, jsonify, request, current_app
+from flask import Blueprint, jsonify, request
 
 from app.models import User, Graph
 from app.extensions import db
@@ -27,7 +27,7 @@ def create_user():
 
     try:
         new_user = User(
-            id=2,  # TODO: swap to UUID
+            id=str(uuid4()),
             username=data['username'],
             email=data['email'],
             password_hash=data['password']  # TODO: hash the password
@@ -68,6 +68,49 @@ def get_user_graphs(user_id):
     return jsonify({'graph_ids': graph_ids}), 200
 
 
+@api_bp.route('/api/user/<int:user_id>/graph', methods=['POST'])
+def create_user_graph(user_id):
+    data = request.get_json()
+
+    if not data:
+        return jsonify({"error": "Missing required fields"}), 400
+
+    try:
+        new_graph = Graph(
+            id=str(uuid4()),
+            name=data['name'],
+            user_id=user_id,
+            data=data['data']
+        )
+
+        db.session.add(new_graph)
+        db.session.commit()
+
+        return jsonify({
+            "message": "Graph created successfully",
+            "graph_id": new_graph.id
+        }), 201
+
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"error": str(e)}), 500
+    
+
+@api_bp.route('/api/user/<int:user_id>/graph/<int:graph_id>/', methods=['DELETE'])
+def delete_user_graph(user_id, graph_id):
+    graph = Graph.query.filter_by(user_id=user_id, id=graph_id).first()
+    if not graph:
+        return jsonify({"error": "Graph not found"}), 404
+
+    try:
+        db.session.delete(graph)
+        db.session.commit()
+        return jsonify({"message": "Graph deleted successfully"}), 200
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"error": str(e)}), 500
+
+
 @api_bp.route('/api/user/<int:user_id>/graph/<int:graph_id>/', methods=['GET'])
 def get_user_graph(user_id, graph_id):
     graph = Graph.query.filter_by(user_id=user_id, id=graph_id).first()
@@ -84,6 +127,28 @@ def get_user_graph(user_id, graph_id):
         return jsonify(graph_data), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+
+
+@api_bp.route('/api/user/<int:user_id>/graph/<int:graph_id>/tsp', methods=['GET'])
+def get_user_graph_tsp(user_id, graph_id):
+    graph = Graph.query.filter_by(user_id=user_id, id=graph_id).first()
+    if not graph:
+        return jsonify({"error": "Graph not found"}), 404
+
+    try:
+        G = nx.DiGraph()
+        G.add_weighted_edges_from(
+            [(edge['from'], edge['to'], edge['weight']) for edge in graph.data['edges']]
+        )
+        if len(G.nodes) < 3:
+            return jsonify({"error": "Graph must have at least 3 nodes"}), 400
+        if not nx.is_strongly_connected(G):
+            return jsonify({"error": "Graph must be strongly connected"}), 400
+        
+        tsp_path = utils.traveling_salesman_path(G)
+        return jsonify({"tsp_path": tsp_path}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
  
 # graph routes
 
@@ -92,49 +157,3 @@ def get_all_graphs():
     graphs = Graph.query.with_entities(Graph.id).all()
     graph_ids = [graph.id for graph in graphs]
     return jsonify({'graph_ids': graph_ids}), 200
-
-
-@api_bp.route('/api/graph/<int:graph_id>/', methods=['GET'])
-def get_graph(graph_id):
-    graph = Graph.query.get(graph_id)
-    if not graph:
-        return jsonify({"error": "Graph not found"}), 404
-
-    try:
-        graph_data = {
-            "id": graph.id,
-            "name": graph.name,
-            "user_id": graph.user_id,
-            "data": graph.data
-        }
-        return jsonify(graph_data), 200
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
-
-
-@api_bp.route('/api/graph/', methods=['POST'])
-def create_graph():
-    data = request.get_json()
-
-    if not data:
-        return jsonify({"error": "Missing required fields"}), 400
-
-    try:
-        new_graph = Graph(
-            id=2,  # TODO: swap to UUID
-            name=data['name'],
-            user_id=data['user_id'],
-            data=data['data']
-        )
-
-        db.session.add(new_graph)
-        db.session.commit()
-
-        return jsonify({
-            "message": "Graph created successfully",
-            "graph_id": new_graph.id
-        }), 201
-
-    except Exception as e:
-        db.session.rollback()
-        return jsonify({"error": str(e)}), 500
